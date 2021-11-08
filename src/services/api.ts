bad20f57da7d96977fbd8888a70c05a44e426e5d
fcursino <fabriciocursino09@gmail.com>
2021-11-08 11:26:26 -0300

    Trocando imagens

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: 'https://3ab4-2804-431-c7db-a1c4-8f3-7352-c123-c639.ngrok.io'
+    baseURL: 'https://6d2f-2804-431-c7db-a1c4-79c8-ab63-5d05-7a20.ngrok.io'
 });
 
 export default api;
